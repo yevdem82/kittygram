329acc904cb3951f7cbc6a9dff15a9f6e2f55fa0
yevdem82 <evdem82@yandex.ru>
2023-04-07 20:52:05 +0300

    Add cat_detail to views

--- cats/views.py
+++ cats/views.py
@@ -17,3 +17,19 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, id):
+    cat = Cat.objects.get(id=id)
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = CatSerializer(cat, request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer = CatSerializer(cat)
+    return Response(serializer.data)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
-from cats.views import cat_list
+from cats.views import cat_list, cat_detail
 
 urlpatterns = [
    path('cats/', cat_list),
+   path('cats/<int:id>', cat_detail)
 ]
 
 
