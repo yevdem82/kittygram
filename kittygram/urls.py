db4a4a5b463c01c48f2e01f480263df10bb5c48d
yevdem82 <evdem82@yandex.ru>
2023-04-18 15:31:03 +1000

    Modify CatViewSet

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,12 @@
+from rest_framework.routers import SimpleRouter
+
 from django.urls import include, path
 
-from cats.views import cat_list
+from cats.views import CatViewSet
+
+router = SimpleRouter()
+router.register('cats', CatViewSet)
 
 urlpatterns = [
-   path('cats/', cat_list),
+    path('', include(router.urls)),
 ]
-
-
